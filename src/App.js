e62bdf25bc28421bdfbe34cf5b996f6ff5dadb1a
Victor Hok <victorhok@outlook.com>
2020-11-29 17:45:21 -0500

    Minor class name tweaking, firebase setup, and ability to push data. :abacus:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
 import { Component } from 'react';
 import { ReactComponent as Mascot } from './assets/duck-mascot.svg' // Just wanted to experiment with importing svgs as Components
+import firebase from './firebase';
 import './styles/style.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      issueList: [],
+      /**
+       * bugList: [ {issue} ]
+       * issue = {
+       * title: <title>,
+       * details: <details>,
+       * status: <status>
+       * }
+       * */ 
+
+      // Placeholders to "grab" current value.
+      issueTitle: '',
+      issueDetails: ''
+    };
+  }
+
+  submitIssueHandler = (event) => {
+    event.preventDefault();
+    // console.log(this.state.issueTitle);
+    // console.log(this.state.issueDetails);
+
+    const dbRef = firebase.database().ref();
+
+    dbRef.push({
+      title: this.state.issueTitle,
+      details: this.state.issueDetails,
+      status: "open"
+    });
+
+    this.setState({
+      issueTitle: '',
+      issueDetails: ''
+    })
+
+  }
+
+  inputIssueTitleHandler = (event) => {
+    this.setState({
+      issueTitle: event.target.value
+    });
+  }
+
+  inputIssueDetailsHandler = (event) => {
+    this.setState({
+      issueDetails: event.target.value
+    });
+  }
+
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+
+    dbRef.on('value', (data) => {
+      console.log(data.val());
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -21,17 +83,17 @@ class App extends Component {
           <div className="wrapper">
             <nav>
               <ul>
-                <li><button>Submit Bug</button></li>
+                <li><button>Submit Issue</button></li>
                 <li><button>View Report</button></li>
               </ul>
             </nav>
             <div className="submit">
-              <form className="submit__form">
+              <form className="submit__form" onSubmit={ this.submitIssueHandler }>
                 <h2>A new bug has been discovered in the wild! 🔍</h2>
-                <label htmlFor="submit__input-title">Bug Title</label>
-                <input type="text" id="submit__input-title" />
-                <label htmlFor="submit__input-description">Details</label>
-                <textarea id="submit__input-description" cols="30" rows="10"></textarea>
+                <label htmlFor="submit__input-title">Issue Title</label>
+                <input type="text" id="submit__input-title" required onChange={this.inputIssueTitleHandler} value={this.state.issueTitle}/>
+                <label htmlFor="submit__input-details">Details</label>
+                <textarea id="submit__input-details" cols="30" rows="10" required onChange={this.inputIssueDetailsHandler} value={this.state.issueDetails}></textarea>
                 <button type="submit">Submit</button>
                 {/* NEED TO PROVIDE ID BACK TO USER AT SOME POINT FOR REFERENCE */}
               </form>
@@ -39,17 +101,17 @@ class App extends Component {
 
             <div className="report">
               <ul>
-                <li>Bug 1</li>
-                <li>Bug 2</li>
-                <li>Bug 3</li>
+                <li><button>Issue 1</button></li>
+                <li><button>Issue 2</button></li>
+                <li><button>Issue 3</button></li>
               </ul>
               <form>
                 <label htmlFor="report__input-id">ID</label>
-                <input type="text" id="report__input-id"/>
+                <input type="text" id="report__input-id" readOnly />
                 <label htmlFor="report__input-title">Title</label>
                 <input type="text" id="report__input-title"/>
-                <label htmlFor="report__input-description">Description</label>
-                <textarea id="report__input-description" cols="30" rows="10"></textarea>
+                <label htmlFor="report__input-details">Details</label>
+                <textarea id="report__input-details" cols="30" rows="10"></textarea>
                 <label htmlFor="report__input-status">Status</label>
                 <select id="report__select-status">
                   <option value="open">Open</option>
