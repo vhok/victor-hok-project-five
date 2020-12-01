0c4dbea5d9cdd9ee2d3117a90732c71d13912c43
Victor Hok <victorhok@outlook.com>
2020-11-30 22:43:14 -0500

    Refactored 3 inputEditHandlers callback functions into just 1. :card_file_box:

--- src/IssueReport.js
+++ src/IssueReport.js
@@ -20,8 +20,9 @@ class IssueReport extends Component {
         issueSelected: {
             title: '',
             details: '',
-            date: '',
-            status: ''
+            dateOpened: '',
+            status: '',
+            // response: ''
         }
         };
     }
@@ -38,27 +39,11 @@ class IssueReport extends Component {
         this.setState({ issueSelectedId: dataKey, issueSelected: issueObj});
     }
 
-    inputEditTitleHandler = (event) => {
-        // Use updater function
+    inputEditHandler = (event, property) => {
+        // Use updater function to modify the issueSelected Object that resides in the state object.
         this.setState( prevState => {
             const issueSelected = { ...prevState.issueSelected };
-            issueSelected.title = event.target.value;
-            return { issueSelected };
-        });
-    }
-
-    inputEditDetailsHandler = (event) => {
-        this.setState(prevState => {
-            const issueSelected = { ...prevState.issueSelected };
-            issueSelected.details = event.target.value;
-            return { issueSelected };
-        });
-    }
-
-    inputEditStatusHandler = (event) => {
-        this.setState(prevState => {
-            const issueSelected = { ...prevState.issueSelected };
-            issueSelected.status = event.target.value;
+            issueSelected[property] = event.target.value;
             return { issueSelected };
         });
     }
@@ -84,7 +69,7 @@ class IssueReport extends Component {
                     id: id,
                     title: firebaseDataObj[id].title,
                     details: firebaseDataObj[id].details,
-                    date: firebaseDataObj[id].date,
+                    dateOpened: firebaseDataObj[id].dateOpened,
                     status: firebaseDataObj[id].status
                 });
             }
@@ -113,14 +98,14 @@ class IssueReport extends Component {
                     {/* value={this.state.issueSelected === '' ? '' : this.state.issueSelected} */}
                     <label htmlFor="report__input-id">ID</label>
                     <input type="text" id="report__input-id" value={this.state.issueSelectedId} readOnly />
-                    <label htmlFor="report__input-date">Date</label>
-                    <input type="datetime" id="report__input-date" value={this.state.issueSelected.date ? new Date(this.state.issueSelected.date).toLocaleString() : ''} readOnly />
+                    <label htmlFor="report__input-date-opened">Date Opened</label>
+                    <input type="datetime" id="report__input-date-opened" value={this.state.issueSelected.dateOpened ? new Date(this.state.issueSelected.dateOpened).toLocaleString() : ''} readOnly />
                     <label htmlFor="report__input-title">Title</label>
-                    <input type="text" id="report__input-title" onChange={this.inputEditTitleHandler} value={this.state.issueSelected.title} />
+                    <input type="text" id="report__input-title" onChange={(event) => this.inputEditHandler(event, 'title')} value={this.state.issueSelected.title} />
                     <label htmlFor="report__input-details">Details</label>
-                    <textarea id="report__input-details" onChange={this.inputEditDetailsHandler} value={this.state.issueSelected.details} cols="30" rows="10"></textarea>
+                    <textarea id="report__input-details" onChange={(event) => this.inputEditHandler(event, 'details')} value={this.state.issueSelected.details} cols="30" rows="10"></textarea>
                     <label htmlFor="report__input-status">Status</label>
-                    <select id="report__select-status" onChange={this.inputEditStatusHandler} value={this.state.issueSelected.status}>
+                    <select id="report__select-status" onChange={(event) => this.inputEditHandler(event, 'status')} value={this.state.issueSelected.status}>
                         <option value="open">Open</option>
                         <option value="wip">In Progress</option>
                         <option value="closed">Closed</option>
